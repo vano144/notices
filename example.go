a88d331a34ec37999ccdd24ccaaddeac101e5da3
Ivan <justworkkit@gmail.com>
2015-08-05 17:17:48 +0300

    Add Authentification
    func BasicAuth

--- example.go
+++ example.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/base64"
 	"flag"
 	"html/template"
 	"log"
@@ -10,8 +11,13 @@ import (
 	"sync"
 )
 
+type Notice struct {
+	Note  string
+	Owner string
+}
+
 type notices struct {
-	Store []string
+	Store []Notice
 	sync.Mutex
 }
 
@@ -20,13 +26,13 @@ var t *template.Template
 
 func main() {
 	var err1 error
-	StoreNotices.Store = make([]string, 0)
+	StoreNotices.Store = make([]Notice, 0)
 	file := path.Join("html", "disignFile.html")
 	t, err1 = template.ParseFiles(file)
 	if err1 != nil {
 		log.Fatal("problem with parsing file", err1)
 	}
-	http.HandleFunc("/message", homePage)
+	http.HandleFunc("/message/", homePage)
 	port := flag.String("port", ":9111", "port in server")
 	flag.Parse()
 	if err4 := http.ListenAndServeTLS(*port, "cert.pem", "key.pem", nil); err4 != nil {
@@ -34,28 +40,57 @@ func main() {
 	}
 }
 
-func homePage(writer http.ResponseWriter, request *http.Request) {
-	writer.Header().Set("Content-type", "text/html")
-	err := request.ParseForm()
+func BasicAuth(w http.ResponseWriter, r *http.Request) (bool, string) {
+	s := strings.SplitN(r.Header.Get("Authorization"), " ", 2)
+	if len(s) != 2 {
+		return false, ""
+	}
+
+	b, err := base64.StdEncoding.DecodeString(s[1])
 	if err != nil {
-		log.Fatal("Problem with parsing form", err)
+		return false, ""
 	}
-	reqSend := request.PostFormValue("sendButton")
-	if reqSend != "" {
-		if slice, found := request.Form["Notice"]; found && len(slice) > 0 {
-			s := ""
-			s = strings.Join(slice, "")
-			StoreNotices.Lock()
-			StoreNotices.Store = append(StoreNotices.Store, s)
-			StoreNotices.Unlock()
-		}
-	} else {
-		reqDel := request.PostFormValue("deleteButton")
-		if reqDel != "" {
-			StoreNotices.Lock()
-			StoreNotices.Store = make([]string, 0)
-			StoreNotices.Unlock()
-		}
+
+	pair := strings.SplitN(string(b), ":", 2)
+	if len(pair) != 2 {
+		return false, ""
 	}
-	t.Execute(writer, StoreNotices)
+	if pair[0] == "invtest" {
+		return false, ""
+	}
+	return true, pair[0]
+}
+
+func homePage(writer http.ResponseWriter, request *http.Request) {
+	flg, name := BasicAuth(writer, request)
+	if flg {
+		writer.Header().Set("Content-type", "text/html")
+		err := request.ParseForm()
+		if err != nil {
+			log.Fatal("Problem with parsing form", err)
+		}
+		reqSend := request.PostFormValue("sendButton")
+		if reqSend != "" {
+			if slice, found := request.Form["Notice"]; found && len(slice) > 0 {
+				s := ""
+				s = strings.Join(slice, "")
+				var k Notice
+				k.Note = s
+				k.Owner = name
+				StoreNotices.Lock()
+				StoreNotices.Store = append(StoreNotices.Store, k)
+				StoreNotices.Unlock()
+			}
+		} else {
+			reqDel := request.PostFormValue("deleteButton")
+			if reqDel != "" {
+				StoreNotices.Lock()
+				StoreNotices.Store = make([]Notice, 0)
+				StoreNotices.Unlock()
+			}
+		}
+		t.Execute(writer, StoreNotices)
+	}
+	writer.Header().Set("WWW-Authenticate", `Basic realm="protectedpage, invalid name for test- invtest"`)
+	writer.WriteHeader(401)
 }
